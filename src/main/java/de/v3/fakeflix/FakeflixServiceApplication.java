9b5399042336e1845fd03e0f3757398bdd460b25
V3kta <luca.rasche@outlook.com>
2020-10-13 11:12:40 +0200

    first commit

---
+++ src/main/java/de/v3/fakeflix/FakeflixServiceApplication.java
@@ -0,0 +1,13 @@
+package de.v3.fakeflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeflixServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeflixServiceApplication.class, args);
+	}
+
+}
